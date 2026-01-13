68530ba5a1754c22db4113d015e1f58605dba74d
Anuchethan T C <79865880+anuchethan@users.noreply.github.com>
2026-01-13 15:08:16 +0000

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Function declarations (prototypes) */
+void pal(void);
+void biggest3(void);
+void factorial(void);
+
+int main(void)
+{
+    pal();
+    biggest3();
+    factorial();
+    return 0;
+}
+
